19afb128116dcac0cbe11f9fc2285dbddced6991
Anandvaghela04 <anandvaghela343@gmail.com>
2025-07-15 12:54:24 +0530

    add new file

---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "abc@abc.com";
+var accountPass = "21211";
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountState]);
+
